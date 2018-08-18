cdfef0f6fd1fd1507b7f18bfb1ef75e7a2d8e2b2
Richard Brockie <richard@ontheday.net>
2018-08-18 16:42:43 -0700

    Fixing a default error!

--- stutter_scroll.js
+++ stutter_scroll.js
@@ -14,7 +14,7 @@ let stutter_pause = 1;
 let stutter_class = 'stutter_scroll_stop_here';
 
 // let url_list = 'https://ontheday.net/\nhttps://google.com/';
-let url_list = 'http://127.0.0.1:8000/2018/bay_climb/tournament/open_men/\nhttp://127.0.0.1:8000/2018/bay_climb/otd_ftw/\nhttp://127.0.0.1:8000/2018/bay_climb/tournament/men_fixed/http://127.0.0.1:8000/2018/bay_climb/otd_ftw\nhttp://127.0.0.1:8000/2018/bay_climb/tournament/open_women/\nhttp://127.0.0.1:8000/2018/bay_climb/otd_ftw/\nhttp://127.0.0.1:8000/2018/bay_climb/tournament/women_fixed/\nhttp://127.0.0.1:8000/2018/bay_climb/otd_ftw/';
+let url_list = 'http://127.0.0.1:8000/2018/bay_climb/tournament/open_men/\nhttp://127.0.0.1:8000/2018/bay_climb/otd_ftw/\nhttp://127.0.0.1:8000/2018/bay_climb/tournament/men_fixed/\nhttp://127.0.0.1:8000/2018/bay_climb/otd_ftw\nhttp://127.0.0.1:8000/2018/bay_climb/tournament/open_women/\nhttp://127.0.0.1:8000/2018/bay_climb/otd_ftw/\nhttp://127.0.0.1:8000/2018/bay_climb/tournament/women_fixed/\nhttp://127.0.0.1:8000/2018/bay_climb/otd_ftw/';
 
 let split_urls = 'tbd';
 let url_index = 0;
